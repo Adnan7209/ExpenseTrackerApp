3e7200f21ce87a32b496188498bad0c1232863d3
Adnan7209 <mdadnan28713@gmail.com>
2024-02-06 00:27:03 +0530

    basic structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
+
+import Remaining from './components/Remaining'
+import Expenditure from './components/Expenditure'
+import ExpensesList from './components/ExpensesList'
+import AddExpenseForm from './components/AddExpenseForm'
+import Budget from './components/Budget'
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box>
+      <Typography>
+        <h1>Expense Tracker</h1>
+      </Typography>
+      <Stack>
+        <Budget/>
+        <Remaining/>
+        <Expenditure/>
+      </Stack>
+      <ExpensesList/>
+      <AddExpenseForm/>
+    </Box>
   )
 }
 
---
+++ src/components/Budget.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Budget = () => {
+  return (
+    <div>budget</div>
+  )
+}
+
+export default Budget
